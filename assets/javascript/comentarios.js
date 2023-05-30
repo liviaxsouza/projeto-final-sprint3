3ec3d3971789590c25cbcecb6369eaf15661b1d5
liviaxsouza <liviasouza.senai564@gmail.com>
2023-05-30 10:39:53 -0300

    fix 29

--- assets/javascript/comentarios.js
+++ assets/javascript/comentarios.js
@@ -1,4 +1,8 @@
 function art() {
     
 let titulo = document.getElementById("titulo").value;
+let resumo = document.getElementById("resumo").value;
+let categorias = document.getElementById("categorias").value;
+let autor = document.getElementById("autor").value;
+let data = document.getElementById("data").value;
 }
