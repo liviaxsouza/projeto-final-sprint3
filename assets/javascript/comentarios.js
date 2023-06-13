9dfd2c111a826a511e68a5237df11bf3ab71b2f0
Nicolly Santos <nicollysilva.senai564@gmail.com>
2023-06-13 13:44:28 -0300

    js2

--- assets/javascript/comentarios.js
+++ assets/javascript/comentarios.js
@@ -2,8 +2,8 @@ function art() {
     
  titulo = document.getElementById("titulo").value;
  resumo = document.getElementById("resumo").value;
- categorias = document.getElementById("categorias").value;
  autor = document.getElementById("autor").value;
  data = document.getElementById("data").value;
+ categorias = document.getElementById("categorias").value;
 }
 
